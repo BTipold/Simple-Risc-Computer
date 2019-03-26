937f291990921cfd34c1deba66b1b4bdb7689a38
Brian Tipold <btipold@gmail.com>
2019-03-26 17:52:42 -0400

    Update instruction_register.vhd

--- VHDL/instruction_register.vhd
+++ VHDL/instruction_register.vhd
@@ -3,16 +3,16 @@ USE IEEE.STD_LOGIC_1164.ALL;
 
 ENTITY instruction_register IS 
 	PORT(
-		D	 		: IN  STD_LOGIC_VECTOR(31 DOWNTO 0);
+		D	: IN  STD_LOGIC_VECTOR(31 DOWNTO 0);
 		
 		opcode	: OUT STD_LOGIC_VECTOR(4  DOWNTO 0);
-		ra	 		: OUT STD_LOGIC_VECTOR(3  DOWNTO 0);
-		rb	 		: OUT STD_LOGIC_VECTOR(3  DOWNTO 0);
-		rc	 		: OUT STD_LOGIC_VECTOR(3  DOWNTO 0);
+		ra	: OUT STD_LOGIC_VECTOR(3  DOWNTO 0);
+		rb	: OUT STD_LOGIC_VECTOR(3  DOWNTO 0);
+		rc	: OUT STD_LOGIC_VECTOR(3  DOWNTO 0);
 		value_i	: OUT STD_LOGIC_VECTOR(31 DOWNTO 0);
-		en  		: IN  STD_LOGIC;
-		clk 		: IN  STD_LOGIC;
-		clr 		: IN  STD_LOGIC
+		en  	: IN  STD_LOGIC;
+		clk 	: IN  STD_LOGIC;
+		clr 	: IN  STD_LOGIC
 	);
 END instruction_register;
 
@@ -20,22 +20,24 @@ ARCHITECTURE arch OF instruction_register IS
 BEGIN
 	PROCESS(clk, clr, en, D)
 	BEGIN
-		IF(clr = '1') THEN
-			opcode <= "00000";
-			ra		 <= "0000";
-			rb		 <= "0000";
-			rc		 <= "0000";
-			value_i<= x"00000000";
-		ELSIF(rising_edge(clk)) THEN
-			IF( en = '1') THEN
-				opcode <= D(31 DOWNTO 27);
-				ra		 <= D(26 DOWNTO 23);
-				rb		 <= D(22 DOWNTO 19);
-				rc		 <= D(18 DOWNTO 15);
-				IF	D(18) = '1' THEN
-					value_i 		<= x"FFFFFFFF";
+		IF (clr = '1') THEN
+			opcode 	<= "00000";
+			ra	<= "0000";
+			rb	<= "0000";
+			rc	<= "0000";
+			value_i	<= x"00000000";
+		ELSIF (rising_edge(clk)) THEN
+			IF (en = '1') THEN
+				opcode	<= D(31 DOWNTO 27);
+				ra	<= D(26 DOWNTO 23);
+				rb	<= D(22 DOWNTO 19);
+				rc	<= D(18 DOWNTO 15);
+
+				-- Sign Extension
+				IF (D(18) = '1') THEN
+					value_i <= x"FFFFFFFF";
 				ELSE
-					value_i 		<= x"00000000";
+					value_i <= x"00000000";
 				END IF;
 				value_i(18 DOWNTO 0) <= D(18 DOWNTO 0);
 			END IF;	
